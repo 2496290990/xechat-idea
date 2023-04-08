beb96c0e91db8368af856c67c15c051eae38ed19
2496290990@qq.com <2496290990@qq.com>
2023-04-08 22:37:32 +0800

    添加游戏模式

---
+++ xechat-plugin/src/main/java/cn/xeblog/plugin/game/uno/enums/GameMode.java
@@ -0,0 +1,18 @@
+package cn.xeblog.plugin.game.uno.enums;
+
+/**
+ * @author Administrator
+ * @date 2023年4月8日22:34:46
+ * @apiNote UNO游戏模式
+ */
+
+public enum GameMode {
+    /**
+     * 经典
+     */
+    CLASSIC,
+    /**
+     * 欢乐模式
+     */
+    HAPPY
+}
---
+++ xechat-plugin/src/main/java/cn/xeblog/plugin/game/uno/enums/PlayerMode.java
@@ -0,0 +1,18 @@
+package cn.xeblog.plugin.game.uno.enums;
+
+/**
+ * 玩家模式
+ *
+ * @author Administrator
+ * @date 2023/04/08
+ */
+public enum PlayerMode {
+    /**
+     * 单人模式
+     */
+    SINGLE,
+    /**
+     * 双人模式
+     */
+    DOUBLE
+}
